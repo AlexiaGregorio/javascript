88745b3dbca428aec0e61fc0011ff9074347fc61
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-01-13 14:33:30 -0300

    ex

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,10 +1,19 @@
 var agora = new Date()
 var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
+/*console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12) {
     console.log(`Bom dia!`)
 } else if (hora <= 18) {
     console.log(`Boa Tarde!`)
 } else { 
     console.log(`Boa noite!`)
+}*/
+if (hora<6){
+    console.log('Boa madrugada!')
+} else if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora < 18) {
+    console.log('Boa tarde!')
+} else { 
+    console.log('Boa noite!')
 }
