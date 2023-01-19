da8059d8da123563b12f243adf42f515d7270e23
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-01-19 14:45:33 -0300

    Tabuada

--- aula 14 - Exercicios/ex017/modelo/script.js
+++ aula 14 - Exercicios/ex017/modelo/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+     window.alert('Por favor, digite um número.')  
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' //para limpar a tabuada depois
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //p outras linguagens isso faz sentido(php), para saber qual item foi selecionado
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+
+}
